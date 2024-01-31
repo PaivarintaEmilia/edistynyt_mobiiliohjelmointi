6b9a86c882912583bb10eb2c03c0d6ae9fd31a8c
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-01-31 18:29:39 +0100

    Toisen tunnin 31.1. tunnilla tehdyt asiat.

--- app/src/main/java/com/example/edistynyttoinentunti/MainActivity.kt
+++ app/src/main/java/com/example/edistynyttoinentunti/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.edistynyttoinentunti
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.edistynyttoinentunti.ui.theme.EdistynytToinenTuntiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginScreen() // Näin saadaan luotu composable näytölle
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+// Columnissa voidaan keskittää kaikki sisällä olevat elementit seuraavilla parametreilla
+// Alt + Enter tekee importin
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    EdistynytToinenTuntiTheme {
-        Greeting("Android")
+fun LoginScreen(){
+    val username = remember{
+        mutableStateOf("")
+    }
+    val password = remember {
+        mutableStateOf("")
+    }
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        OutlinedTextField(value = username.value, onValueChange = { newUsername ->
+                username.value = newUsername
+        }, placeholder = {
+            Text(text = "Username")
+        }) //tämä on tekstikenttä, johon syötetään käyttäjätunnus
+        Spacer(modifier = Modifier.height(16.dp)) // Saadaan väliä
+        OutlinedTextField(value = password.value, onValueChange = {
+                                                                  newPassword ->
+                                                                  password.value = newPassword
+        }, placeholder = {
+            Text(text = "Password")
+                         })
     }
 }
+
