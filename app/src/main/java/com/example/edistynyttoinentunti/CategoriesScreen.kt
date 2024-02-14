39d41e0f0dc5cd80c5340fad4c86115fab4b46f6
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-02-14 18:22:24 +0100

    12.2. Tunnin ensimmäinen puolisko. Aloitettiin kategorioiden lisääminen.

--- app/src/main/java/com/example/edistynyttoinentunti/CategoriesScreen.kt
+++ app/src/main/java/com/example/edistynyttoinentunti/CategoriesScreen.kt
@@ -3,6 +3,7 @@ package com.example.edistynyttoinentunti
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,14 +14,33 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.edistynyttoinentunti.viewmodel.CategoriesViewModel
 
+//Näin saadaan preview näkymä käyttöön, muuten ei tarpeellinen
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CategoriesScreenPreview() {
 
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "Categories") }, navigationIcon = {
+            IconButton(onClick = {  }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+            }
+        } )
+    }) {
+        LazyColumn(modifier = Modifier.padding(it)) {}
+    }
+}
 // Tämä tehtiin vain, jotta voidaan tarkastella navigaatiota
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoriesScreen(onMenuClick: () -> Unit) {
     // Tämä on rakennusteline, joka antaa navigaatiolle raamit
+    val categoriesVm: CategoriesViewModel = viewModel()
     // Tähän voi itse sitten lisäillä omia osia kuten topBarin
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "Categories") }, navigationIcon = {
@@ -29,7 +49,10 @@ fun CategoriesScreen(onMenuClick: () -> Unit) {
             }
         } )
     }) {
-        LazyColumn(modifier = Modifier.padding(it)) {}
+        LazyColumn(modifier = Modifier.padding(it)) {
+            // Tähän tulee saada itemit CategoriesStatesta, joka sijaitsee CategoriesViewModelissa.
+
+        }
         // Tämä tehtiin vain, jotta saadaan virheilmoitus pois
     }
 }
--- app/src/main/java/com/example/edistynyttoinentunti/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/example/edistynyttoinentunti/viewmodel/CategoriesViewModel.kt
@@ -1,8 +1,55 @@
 package com.example.edistynyttoinentunti.viewmodel
 
+import androidx.compose.runtime.Recomposer
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.edistynyttoinentunti.model.CategoriesState
+import com.example.edistynyttoinentunti.model.CategoryItem
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CategoriesViewModel : ViewModel() {
 
-    // Tänne yhdistetään seuraavalla tunnilla Categories.kt tiedostossa olevat classit
+
+    // Tänne yhdistetään seuraavalla tunnilla Categories.kt tiedostossa olevat luokat
+    //  Tässä pitää näkyä lista kategorioista, joita meillä tulee olemaan meidän softassa
+
+
+    private val _categoriesState = mutableStateOf(CategoriesState())
+
+    // Julkinen vastine jotta voidaan käyttää composablessa
+    val categoriesState: State<CategoriesState> = _categoriesState
+
+    init {
+        // Init on blokki, joka voidaan ajaa primary contruktorin ajamisen jälkeen.
+        // Täällä tehdään myös rajapintakutsut. Tämä pyöritetään, heti näytön käynnistyttyä. (Tälle näytölle haetaan dataa tietokannasta.)
+        // Täällä voidaan kutsua getCategories()-fukntiota ilman, että tulee ikilooppi.
+        getCategories()
+    }
+
+
+    private suspend fun waitForCategories() {
+        delay(2000)
+    }
+
+
+    // Haetaan kategorioita
+    private fun getCategories() {
+
+        viewModelScope.launch {
+            _categoriesState.value = _categoriesState.value.copy(loading = true) // Tämä on false Categories.kt puolella
+            waitForCategories() // Odotetaan 2 sekunttia
+            // Data tulee sisään. Huom käytetään luokkaa Categories.kt puolelta
+            _categoriesState.value = categoriesState.value.copy(
+                loading = false,
+                list = listOf(
+                    CategoryItem(id = 1, name = "Kateogoria1"),
+                    CategoryItem(id = 2, name = "Kategoria2"))
+            )
+        }
+    }
+
+
 }
